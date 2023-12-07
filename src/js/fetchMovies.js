d4bc0f7928fae5efae60f554760a58ccb5b62f05
robertSovar <133514926+robertSovar@users.noreply.github.com>
2023-12-07 21:36:38 +0200

    update fetchMovies.js
    Created the function that fetches the movies

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,2 +1,18 @@
-// FT-07 Realizează încărcarea filmelor populare pe pagina principală (prima)
-// se va folosi functie asyncrona - try - catch
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const API_KEY = 'a5406dd14816b26728050ce2e3dfdd5f';
+const ENDPOINT = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${API_KEY}`;
+
+export async function fetchMovies() {
+  try {
+    const res = await axios.get(ENDPOINT);
+    console.dir(res.data)
+    return res.data;
+  } catch (error) {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+    throw error;
+  }
+};
