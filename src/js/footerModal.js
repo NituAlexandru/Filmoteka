dd6525d8a7638b2ca07a7e758ad9af1c14595037
Sebastiand22 <sebastiandunduc16@gmail.com>
2023-12-10 15:02:38 +0200

    first commit

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -1,3 +1,37 @@
 // FT - 23 Creează o fereastră modală(mobile, tablet, desktop) cu
 // informații despre membrii echipei tale, care se va deschide la click
 // pe textul "GoIT Students" din footer.
+
+var modal = document.getElementById('myFooterModal');
+
+var btn = document.getElementById('openFooterModal');
+
+var span = document.getElementsByClassName('close-button-member')[0];
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+document
+  .querySelectorAll('.member-links .social-links')
+  .forEach(function (link) {
+    link.addEventListener('click', function () {
+      var svg = this.querySelector('svg');
+      svg.style.fill = 'white';
+    });
+
+    link.addEventListener('click', function (event) {
+      var link = this.querySelector('a').getAttribute('href');
+      window.location.href = link;
+    });
+  });
