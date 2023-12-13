67315958fd05e3b4e134d1c5a939c764e9b0b324
RoxanaLarisssa <roxana.larisssa@gmail.com>
2023-12-13 22:21:59 +0000

    update header css

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,1 +1,14 @@
 //salvarea in local storage a listelor
+const searchInput = document.qetElementbyId('searchInput');
+
+searchInput.addEventListener('change', function () {
+  const query = searchInput.value;
+
+  localStorage.setItem('lastSearchQuery', query);
+});
+
+const lastSearchQuery = localStorage.getItem('lastSearchQuery');
+
+if (lastSearchQuery) {
+  searchInput.value = lastSearchQuery;
+}
