ef7d2fe9f1987d7c252c569f5290f1d858318a09
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2023-12-13 21:03:13 +0200

    createFilmCard optimization
    -added more rules for genre type display
    -for title
    -release year

--- src/js/createFilmCard.js
+++ src/js/createFilmCard.js
@@ -7,41 +7,69 @@ export async function createFilmCard(dataPromise) {
   movieCard.textContent = '';
 
   if (!data || !data.results) {
-    // console.error('Datele nu sunt disponibile sau nu au proprietatea "results".');
+    console
+      .error
+      // 'Datele nu sunt disponibile sau nu au proprietatea "results".'
+      ();
     return;
   }
 
   data.results.forEach(response => {
+    // Crează un element nou pentru fiecare film/serial
     const movieElement = document.createElement('div');
     movieElement.classList.add('movie-wrapper__card');
-    const releaseYear = response.release_date
-      ? response.release_date.split('-')[0]
-      : 'N/A';
-    const rating = typeof response.vote_average === 'number' ? response.vote_average : 'N/A'; // Presupunând că nota filmului este în response.vote_average
-    const genreNames = response.genre_ids
-      .map(id => genres.find(genre => genre.id === id)?.name || 'Unknown')
-      .join(', ');
-    movieElement.innerHTML = `
-        
-        <div class="movie-wrapper__card-img">
-          <img src="https://image.tmdb.org/t/p/w500${
-            response.poster_path
-          }" alt="${response.title}">
-        </div>
-        <div class="movie-wrapper__footer">
-          <div class="movie-wrapper__title">
-            ${response.title}
-          </div>
-          <div class="movie-wrapper__info">
-            <p class="movie-wrapper__info-genres">${genreNames} </p>
-            <span class="movie-wrapper__info-year">| ${releaseYear}</span>
-            <span class="movie-wrapper__info-rating"> ${rating.toFixed(
-              2
-            )}</span>
-          </div>
-        </div>        
-        `;
 
+    // Determină titlul: folosește 'name' pentru seriale și 'original_title' pentru filme
+    let title = 'Unknown';
+    if (response.media_type === 'tv') {
+      title = response.name || response.original_name || 'Unknown';
+    } else if (response.media_type === 'movie') {
+      title = response.title || response.original_title || 'Unknown';
+    }
+
+    // Mapează ID-urile genurilor la numele lor, folosind lista de genuri încărcată
+    const genreNames = response.genre_ids
+      .map(id => {
+        const genre = genres.find(genre => genre.id === id);
+        return genre ? genre.name : 'Unknown';
+      })
+      .filter(name => name !== 'Unknown')
+      .join(', ');
+    // console.log(genres);
+    if (genreNames.includes('Unknown')) {
+      console.log('ID-uri genuri necunoscute:', response.genre_ids);
+      console.log('Lista de genuri disponibile:', genres);
+    }
+
+    // Determină anul de lansare
+    let releaseYear = 'N/A';
+    if (response.media_type === 'movie' && response.release_date) {
+      releaseYear = response.release_date.split('-')[0];
+    } else if (response.media_type === 'tv' && response.first_air_date) {
+      releaseYear = response.first_air_date.split('-')[0];
+    }
+
+    // Formatează rating-ul pentru a afișa doar două zecimale
+    const rating =
+      typeof response.vote_average === 'number'
+        ? response.vote_average.toFixed(2)
+        : 'N/A';
+
+    // Construiește HTML-ul pentru cardul filmului/serialului
+    movieElement.innerHTML = `
+      <div class="movie-wrapper__card-img">
+        <img src="https://image.tmdb.org/t/p/w500${response.poster_path}" alt="${title}">
+      </div>
+      <div class="movie-wrapper__footer">
+        <div class="movie-wrapper__title">${title}</div>
+        <div class="movie-wrapper__info">
+          <p class="movie-wrapper__info-genres">${genreNames}</p>
+          <span class="movie-wrapper__info-year">| ${releaseYear}</span>
+          <span class="movie-wrapper__info-rating">${rating}</span>
+        </div>
+      </div>`;
+
+    // Adaugă elementul creat în containerul principal
     movieCard.append(movieElement);
   });
 }
