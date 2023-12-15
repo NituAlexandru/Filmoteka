c7c4b20e622debfea0b9f1c76e3c16cf2d300b64
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2023-12-16 01:57:48 +0200

    Finished team modal
    -bug fixes
    -adaptive optimization
    -styles

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -6,16 +6,25 @@ const closeBtn = document.querySelector('.close-button-member');
 // Funcție pentru a deschide fereastra modală
 function openModal() {
   modal.style.display = 'block';
+  window.addEventListener('keydown', handleEscapeKeyPress);
 }
 
 // Funcție pentru a închide fereastra modală
 function closeModal() {
   modal.style.display = 'none';
-  event.stopPropagation();
+  window.removeEventListener('keydown', handleEscapeKeyPress);
+}
+
+// Funcție pentru a gestiona apăsarea tastei Escape
+function handleEscapeKeyPress(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
 }
 
 btn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
+
 // Închide fereastra modală dacă utilizatorul dă click în afara acesteia
 window.addEventListener('click', function (event) {
   if (event.target === modal) {
