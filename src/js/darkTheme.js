65bd17a760f0d5c418e19d4c51b13fc1413ea830
Sebastiand22 <sebastiandunduc16@gmail.com>
2023-12-15 16:09:58 +0200

    Update darkTheme.js

--- src/js/darkTheme.js
+++ src/js/darkTheme.js
@@ -1,1 +1,12 @@
 // tema dark - cauta librarie pe net, citeste doc, instaleaza, do it
+document.addEventListener("DOMContentLoaded", function() {
+  // Funcția myFunction
+  function myFunction() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+  }
+
+  // Apelează myFunction atunci când butonul este apăsat
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  darkModeToggle.addEventListener('click', myFunction);
+});
