366bea57f2465087338de8fd2aabd73ed7c299e6
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2023-12-16 00:39:00 +0200

    Footer get current year
    Added auto change to the trademark year

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-
 import { fetchMovies } from './js/fetchMovies.js';
 import { createFilmCard } from './js/createFilmCard.js';
 import { searchForm } from './js/searchForm.js';
 import { modal, btn, span } from './js/footerModal.js';
 import { createPagination } from './js/createPagination.js';
 import { openFilmModal } from './js/openFilmModal.js';
+import footerGetFullYear from './js/footerGetFullYear.js';
 
-
+document.addEventListener('DOMContentLoaded', () => {
+  footerGetFullYear();
+});
