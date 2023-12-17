7c63f920808c8034e7940dc36e9478db6b697977
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2023-12-17 15:33:58 +0200

    Update dark mode btn

--- src/js/darkTheme.js
+++ src/js/darkTheme.js
@@ -1,14 +1,19 @@
 let darkMode = localStorage.getItem('darkMode'); // check for saved 'darkMode' in localStorage
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const darkModeIcon = document.querySelector('#dark-mode-icon');
 
 const enableDarkMode = () => {
   document.body.classList.add('darkmode'); // 1. Add the class to the body
   localStorage.setItem('darkMode', 'enabled'); // 2. Update darkMode in localStorage
+  darkModeIcon.classList.remove('fa-sun'); // 3. Schimbă iconița în soare
+  darkModeIcon.classList.add('fa-moon');
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode'); // 1. Remove the class from the body
   localStorage.setItem('darkMode', null); // 2. Update darkMode in localStorage
+   darkModeIcon.classList.remove('fa-moon');
+   darkModeIcon.classList.add('fa-sun');
 };
 
 // If the user already visited and enabled darkMode
