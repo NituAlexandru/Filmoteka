34cfaeb306c4113234baad04fd723a1ff80c1afe
robertSovar <133514926+robertSovar@users.noreply.github.com>
2023-12-28 19:21:56 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import { fetchMovieTrailer } from './js/fetchMovies.js';
-fetchMovieTrailer();
 import { fetchMovies } from './js/fetchMovies.js';
-fetchMovies();
-import { createFilmCard } from './js/createFilmCard.js';
-createFilmCard();
-
+import { modal, btn, span } from './js/footerModal.js';
+import { createPagination } from './js/createPagination.js';
+import { openFilmModal } from './js/openFilmModal.js';
+import footerGetFullYear from './js/footerGetFullYear.js';
+import { setupMyLibraryLink } from './js/myLibrary.js';
+import { darkmode } from './js/darkTheme.js';
+import { scrollFunction, backToTop } from './js/scrollToTopBtn.js';
 import { searchForm } from './js/searchForm.js';
+
 window.addEventListener('load', async () => {
   try {
+    footerGetFullYear();
+    setupMyLibraryLink();
+    scrollFunction();
+    backToTop();
     const popularMovies = await fetchMovies();
     createPagination(popularMovies);
 
@@ -16,31 +22,3 @@ window.addEventListener('load', async () => {
     // console.error('Eroare la încărcarea filmelor populare:', error);
   }
 });
-
-import { modal, btn, span } from './js/footerModal.js';
-import { createPagination } from './js/createPagination.js';
-import { openFilmModal } from './js/openFilmModal.js';
-
-import footerGetFullYear from './js/footerGetFullYear.js';
-document.addEventListener('DOMContentLoaded', () => {
-  footerGetFullYear();
-});
-
-import { setupMyLibraryLink } from './js/myLibrary.js';
-document.addEventListener('DOMContentLoaded', () => {
-  setupMyLibraryLink();
-});
-
-import { darkmode } from './js/darkTheme.js';
-
-
-import { scrollFunction, backToTop } from './js/scrollToTopBtn.js';
-scrollFunction();
-backToTop();
-
-
-
-
-
-
-
