8a3538bc9813becd9fea754f01a7c067575b6475
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-01-15 21:46:23 +0200

    implementing tui pagination

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { fetchMovies } from './js/fetchMovies.js';
 import { modal, btn, span } from './js/footerModal.js';
-import { createPagination } from './js/createPagination.js';
 import { openFilmModal } from './js/openFilmModal.js';
 import footerGetFullYear from './js/footerGetFullYear.js';
 import { setupMyLibraryLink } from './js/myLibrary.js';
 import { darkmode } from './js/darkTheme.js';
 import { scrollFunction, backToTop } from './js/scrollToTopBtn.js';
 import { searchForm } from './js/searchForm.js';
+import { createFilmCard } from './js/createFilmCard.js';
+import { createPagination } from './js/createPagination.js';
+
+isSearched = true;
 
 window.addEventListener('load', async () => {
   try {
@@ -15,10 +18,9 @@ window.addEventListener('load', async () => {
     scrollFunction();
     backToTop();
     const popularMovies = await fetchMovies();
+    createFilmCard(popularMovies);
     createPagination(popularMovies);
-
-    // console.log('Filme populare:', popularMovies);
   } catch (error) {
-    // console.error('Eroare la încărcarea filmelor populare:', error);
+    console.alert('Eroare la încărcarea filmelor populare:', error);
   }
 });
